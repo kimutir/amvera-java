42b338d92f51f2bc46b25593f3be504ce8273c32
Timur Kim <kimutir@gmail.com>
2023-11-26 12:49:46 +1100

    dirty context after each method

--- src/test/java/com/instruction/amverajava/controller/ProductControllerTest.java
+++ src/test/java/com/instruction/amverajava/controller/ProductControllerTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ProductControllerTest {
 
     @Autowired
